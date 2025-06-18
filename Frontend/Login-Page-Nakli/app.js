e262d037453595b48bf3b343b30e8f4639fa43a8
Sumit-Shastri <shastrisumit25@gmail.com>
2025-06-18 15:10:35 +0530

    matched both files from frontend and backend

--- Frontend/Login-Page-Nakli/app.js
+++
@@ -1,7 +0,0 @@
-const signInbtn = document.querySelector('.signInbtn');
-const signUpbtn = document.querySelector('.signUpbtn');
-const boxx = document.querySelector('.boxx');
-
-signUpbtn.addEventListener('click', () => {
-    boxx.classList.toggle('active');
-})
