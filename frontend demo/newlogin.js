e5389f9b1d5cf62bf9e94d73537e4a20710369be
vijaym105 <malusarevijay10@gmail.com>
2025-08-02 12:38:22 +0530

    Added toggle btn

--- frontend demo/newlogin.js
+++ frontend demo/newlogin.js
@@ -1,3 +1,4 @@
+
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
@@ -10,22 +11,26 @@ signInButton.addEventListener('click', () =>
     container.classList.remove("right-panel-active")
 );
 
-
-let inp = document.querySelector("input");
-inp.addEventListener("input", (ded) => {
-    if (ded.data !== null) {
-        console.log(ded.data);
-    }
+let pass = document.querySelector(".password");
+let name = document.querySelector("#name");
+let form = document.querySelector("form");
+let inputs = document.querySelector("input");
+form.addEventListener("submit",(dets)=>{
+    dets.preventDefault();
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let ans = regex.test("vijay12@domain.co.in")
+    console.log(ans);
+    const regex2 = /^[a-zA-Z0-9_]{3,20}$/;
+    let ans2 = regex2.test(`${inputs.value}`);
+    console.log(ans2);
 });
 
+const toggleBtn = document.getElementById("C");
+let isDark = false;
 
-let btn = document.getElementById("signUp");
-btn.addEventListener("click", function () {
-    alert("button clicked");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  isDark = !isDark;
+  toggleBtn.textContent = isDark ? "Switch to Light Mode" : "Switch to Dark Mode";
 });
-
-let np = document.querySelector("input");
-np.addEventListener("focus", () => {
-    np.style.border = "red 2px";
-})
 
