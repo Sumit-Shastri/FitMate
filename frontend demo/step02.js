58338f3bf06d5cea1bd5e6b996d914727eef2488
vijaym105 <malusarevijay10@gmail.com>
2025-09-03 17:31:58 +0530

    hover on next

--- frontend demo/step02.js
+++ frontend demo/step02.js
@@ -39,11 +39,11 @@ function BmiCal() {
     fnt.style.display = "initial";
     console.log(fnt);
     gsap.from(fnt,{
-        delay: 3,
+        delay: 1,
         duration: 1,
         y:30,
         opacity:0,
-        stagger:0.5,
+        stagger:0.8,
         yoyo:true,
         scrollTrigger:{
         trigger:"fnt",
@@ -52,26 +52,25 @@ function BmiCal() {
     })
 }
 
-const elastic = document.querySelector(".curve");
-
-var path = `M 30 30 Q 100 60 200 60`
-
-var fpath = `M 30 30 Q 100 30 250 30`
 
 
-elastic.addEventListener("mousemove",(dets)=>{
-    path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`
-    console.log(dets)
-    gsap.to("svg path",{
-        attr:{d:path},
-        duration:0.5,
-        ease:"power3.out"
+const next = document.querySelector(".next");
+const hover = document.querySelector(".hvr");
+
+next.addEventListener("mouseenter",()=>{
+    gsap.to(hover,{
+        x:100,
+        opacity:1,
+        zIndex: 1,
+        
+        ease: "power2.out"
     })
 })
-elastic.addEventListener("mouseleave",()=>{
-    gsap.to("svg path",{
-        attr:{d:fpath},
-        duration:0.6,
-        ease:"elastic.out(1.2,0.2)"
+next.addEventListener("mouseleave",()=>{
+    gsap.to(hover,{
+        x:0,
+        opacity:0,
+        zIndex: -1,
+        ease: "power3.out"
     })
 })
